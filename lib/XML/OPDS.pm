b07f693866b6e084e30f2f362aa1a58d9644af50
Marco Pessotto <melmothx@gmail.com>
2016-03-06 14:31:07 +0100

    Bumped version

--- lib/XML/OPDS.pm
+++ lib/XML/OPDS.pm
@@ -18,11 +18,11 @@ XML::OPDS - OPDS (Open Publication Distribution System) feed creation
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 DESCRIPTION
