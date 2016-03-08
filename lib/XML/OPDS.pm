9c86f10e46c411eb5ec10e50b871659a737ebcb9
Marco Pessotto <melmothx@gmail.com>
2016-03-08 09:09:31 +0100

    Version 0.04 on cpan

--- lib/XML/OPDS.pm
+++ lib/XML/OPDS.pm
@@ -18,11 +18,11 @@ XML::OPDS - OPDS (Open Publication Distribution System) feed creation
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 DESCRIPTION
